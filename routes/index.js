230603eec58918f5abff91b82563ca105a073316
arnoldolpn <arnoldoliesenberg@gmailcom>
2019-08-20 19:59:51 -0300

    comitando

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,21 @@ var posts = postsService.getPosts();
   res.render('index', { title: 'Blog', posts: posts });
 });
 
+router.get('/posts/:postId', function(req, res, next){
+  var postId = req.params.postId;
+
+  var posts = postsService.getPosts();
+
+  /*for (var i=0; i< posts.length;; i++){
+    if (posts.[i].id == postid){
+      var post = posts[i];
+    }
+  }*/
+
+  var post = posts.filter((post) => post.id == postId) [0];
+
+  res.render('post', {title: post.title, post: post});
+  
+});
+
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -3,16 +3,25 @@ var getPosts = function ()
     var posts = 
     [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu Primeiro post',
             body: 'Meu Primeiro post blah blah'
         },
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu Segundo post',
             body: 'Meu segundo post blah blah'
+        },
+        {
+            id: 3,
+            title: 'Sistema Automático de Posts',
+            image: 'post2.jpg',
+            description: 'Novo sistema de posts maldito lazarento',
+            body: 'Esse é o novo sistema automático de posts'
         },
     ];
 return posts;
